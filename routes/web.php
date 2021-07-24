182a419e029d3645051364a8495918df1e81e77a
Dev-Armaghan <rasheedarmaghan@gmail.com>
2021-07-24 15:27:09 +0500

    Index

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\ProductController;
 |
 */
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 Route::get('addProducts',function(){
     return view('addProduct');
